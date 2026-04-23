3afdf907207e7ca0345cd9431c9173e3a430ed18
Joatan Feitosa <joatancarlos@gmail.com>
2026-04-23 14:57:30 -0300

    cloning the project from gitlab

--- src/app.js
+++ src/app.js
@@ -58,7 +58,6 @@ app.use('/api/scoreboard', scoreRoutes);
 
 app.use(errorHandler);
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(path.join(__dirname, '../UI'))
